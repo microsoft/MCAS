2b9530bbef7d72eeb6a9bae7c0b786695c0b7467
Jared <jpoeppel@microsoft.com>
2017-08-07 11:52:08 -0400

    Added Get-MCASAdminAccess and fixed .identity aliasing

--- Cloud-App-Security.psm1
+++ Cloud-App-Security.psm1
@@ -361,7 +361,7 @@ function Invoke-MCASRestMethod
                 }
 
             # Add 'Identity' alias property, when appropriate
-            If ($Response._id) {
+            If ((($Response | Get-Member | Select Name) -match '_id').count -gt 0) {
                 $Response = $Response | Add-Member -MemberType AliasProperty -Name Identity -Value _id -PassThru
                 }
             $Response
@@ -2612,6 +2612,50 @@ function Export-MCASBlockScript
                 Throw $_  #Exception handling is in Invoke-MCASRestMethod, so here we just want to throw it back up the call stack, with no additional logic
             }
         $Response = $Response.Content
+        $Response
+    }
+    End
+    {
+    }
+}
+
+function Get-MCASAdminAccess
+{
+    [CmdletBinding()]
+    [Alias('Get-CASAdminAccess')]
+    Param
+    (   
+        # Specifies the URL of your CAS tenant, for example 'contoso.portal.cloudappsecurity.com'.
+        [Parameter(Mandatory=$false)]
+        [ValidateScript({($_.EndsWith('.portal.cloudappsecurity.com') -or $_.EndsWith('.adallom.com'))})]
+        [string]$TenantUri,
+
+        # Specifies the CAS credential object containing the 64-character hexadecimal OAuth token used for authentication and authorization to the CAS tenant.
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$Credential
+    )
+    Begin
+    {
+        $Endpoint = 'manage_admin_access'
+        
+        Try {$TenantUri = Select-MCASTenantUri}
+            Catch {Throw $_}
+
+        Try {$Token = Select-MCASToken}
+            Catch {Throw $_}
+    }
+    Process
+    {            
+        Try 
+        {
+            $Response = Invoke-MCASRestMethod -TenantUri $TenantUri -Endpoint $Endpoint -CASPrefix -Method Get -Token $Token
+        }
+            Catch
+            { 
+                Throw $_  #Exception handling is in Invoke-MCASRestMethod, so here we just want to throw it back up the call stack, with no additional logic
+            }
+        #$Response = $Response.Content
         $Response
     }
     End
