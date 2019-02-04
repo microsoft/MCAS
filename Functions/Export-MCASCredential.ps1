5714edfa20e6f07a3e7a45c4005913a386aed273
powershellshock <jpoeppel@microsoft.com>
2019-02-04 14:37:54 -0500

    new file and related exports

--- Functions/Export-MCASCredential.ps1
+++ Functions/Export-MCASCredential.ps1
@@ -1,5 +1,6 @@
 <#
 .Synopsis
+<<<<<<< refs/remotes/origin/PSCore-nonWindows-CASCredential-handling
    Exports an MCAS credential (usually from the $CASCredential session variable) to a local file
 
 .DESCRIPTION
@@ -10,11 +11,20 @@
 
 .FUNCTIONALITY
    Export-MCASCredential returns nothing.
+=======
+    Exports an MCAS credential object (usually from the $CASCredential variable) to a file on non-Windows machines running Powershell (Core)
+.DESCRIPTION
+    <ADD DESCRIPTION HERE>
+.EXAMPLE
+    PS C:\> Export-MCASCredential
+
+>>>>>>> new file and related exports
 #>
 function Export-MCASCredential {
     [CmdletBinding()]
     param
     (
+<<<<<<< refs/remotes/origin/PSCore-nonWindows-CASCredential-handling
         # Specifies the app for which to retrieve the integer id value.
         [Parameter(Mandatory=$true, ValueFromPipeline=$false, Position=0)]
         [ValidateNotNullOrEmpty()]
@@ -47,4 +57,8 @@ function Export-MCASCredential {
             throw "The following error occurred when trying to export the credential object: $_"
         }
     }
+=======
+    )
+
+>>>>>>> new file and related exports
 }
