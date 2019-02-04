3969b7abb91246d43759935e10fe178dfd0bec01
powershellshock <jpoeppel@microsoft.com>
2019-02-04 14:37:54 -0500

    new file and related exports

---
+++ Functions/Export-MCASCredential.ps1
@@ -0,0 +1,16 @@
+<#
+.Synopsis
+    Exports an MCAS credential object (usually from the $CASCredential variable) to a file on non-Windows machines running Powershell (Core)
+.DESCRIPTION
+    <ADD DESCRIPTION HERE>
+.EXAMPLE
+    PS C:\> Export-MCASCredential
+
+#>
+function Export-MCASCredential {
+    [CmdletBinding()]
+    param
+    (
+    )
+
+}
