fac1970efa1e5bdb005b220f9155709e7b4a6b80
Jared <jpoeppel@microsoft.com>
2019-02-08 14:25:27 -0500

    Export-MCASCredential basic functionality

--- Functions/Export-MCASCredential.ps1
+++ Functions/Export-MCASCredential.ps1
@@ -24,33 +24,48 @@ function Export-MCASCredential {
         # Specifies the app for which to retrieve the integer id value.
         [Parameter(Mandatory=$true, ValueFromPipeline=$false, Position=0)]
         [ValidateNotNullOrEmpty()]
-        [string]$Path,
+        $Path,
         
         # Specifies the app for which to retrieve the integer id value.
         [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
         [ValidateNotNullOrEmpty()]
-        [SecureString] $Credential = $CASCredential
+        $test = $CASCredential
     )
-    {
-        #change for commit
-
-        #$username = 'jpdemo18.portal.cloudappsecurity.com'
-        #$password = 'RV9LSkJAHhcBWlwdAV9AXVtOQwFMQ0BaS05fX1xKTFpdRltWAUxAQlNMGkkWFhYbGhscGkkbG0lLHksaHRoaSkkeHRxOShocGU0WGBpLSUtNG05LTR4XHRkdHxYaFhYaS01LH00eHUoc'
-        #$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+    process {
 
         #$Key = (3,4,2,3,56,34,254,222,1,1,2,23,42,54,33,233,1,34,2,7,6,5,35,43)       
         #$s  | ConvertFrom-SecureString -Key $Key 
 
-        $nonWindowsCredential = New-Object -TypeName psobject -Property @{
-            UserName = $Credential.UserName
-            Password = $Credential.GetNetworkCredential().Password
-        }
+        $uri = $test.UserName
+        $p = ($test.GetNetworkCredential().Password)
+
+        Write-Verbose "Tenant URI is $uri"
+        Write-Verbose "Token is $p"
         
+        #[string]$uri = $CASCredential.UserName
+        #[string]$p = $CASCredential.GetNetworkCredential().Password
+        
+        $nonWindowsCredential = New-Object -TypeName psobject -Property @{
+            UserName = $uri
+            Password = $p
+            
+            #GetNetworkCredential().username
+            #GetNetworkCredential().Passsword
+        }
+
+        #Add-Member ScriptMethod ToString { $this.Name }
+
+        Write-Verbose ($nonWindowsCredential.Password)
+
+        Write-Verbose "Export path is $Path"
+        Export-Clixml -InputObject $nonWindowsCredential -Path $Path
+        <#
         try {
             Export-Clixml -Path $Path -InputObject $nonWindowsCredential
         }
         catch {
             throw "The following error occurred when trying to export the credential object: $_"
         }
+        #>
     }
 }
